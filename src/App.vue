55627b9d2ef117f86ada09e3d97da1a59fe17476
elcin1040 <ebil.rehimov@mail.ru>
2021-03-20 04:23:30 +0400

    first file

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+
+
+<transition-group mode="out-in">
+<component
+class="animate__animated animate__heartBeat"
+@data="activeComponenet=$event"
+:is="activeComponenet"
+key="mainComponent"
+>
+
+</component>
+
+</transition-group>
+
+
+</template>
+
+<script>
+import gameCards from "./components/gameCards";
+import GameCards from './components/gameCards.vue';
+import lost from "./components/lost";
+import win from "./components/win"
+export default {
+  components:{
+gameCards,lost,win
+   
+  },
+ data(){
+   return{
+    activeComponenet:gameCards,
+   }
+ }
+}
+</script>
